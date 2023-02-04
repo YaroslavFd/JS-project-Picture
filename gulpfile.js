23f4d4a4ffed5c4a36ba8cda3dc7bc045ad9aea9
Yaroslav <yaroslav.web9@gmail.com>
2023-02-04 13:19:24 +0300

    added calc

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = 'D:/LocalServer/OSPanel/domains/test';
-// const dist = "./dist/";
+// const dist = 'D:/LocalServer/OSPanel/domains/test';
+const dist = "./dist/";
 
 gulp.task("copy-html", () => {
   return gulp.src("./src/index.html")
